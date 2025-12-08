02c15aa47cc8754c90b133360e8a17ad6d96bd58
unknown <tareqmahmudkhan187@gmail.com>
2025-12-08 16:58:39 +0600

    add payment intregation

---
+++ index.js
@@ -0,0 +1,179 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY) 
+const admin = require('firebase-admin')
+const port = process.env.PORT || 3000
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString(
+  'utf-8'
+)
+const serviceAccount = JSON.parse(decoded)
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+})
+
+const app = express()
+// middleware
+// app.use(
+//   cors({
+//     origin: [process.env.CLIENT_DOMAIN],
+//     credentials: true,
+//     optionSuccessStatus: 200,
+//   })
+// )
+
+app.use(cors({
+  origin: true,   
+  credentials: true,
+}))
+app.use(express.json())
+
+// jwt middlewares
+const verifyJWT = async (req, res, next) => {
+  const token = req?.headers?.authorization?.split(' ')[1]
+  console.log(token)
+  if (!token) return res.status(401).send({ message: 'Unauthorized Access!' })
+  try {
+    const decoded = await admin.auth().verifyIdToken(token)
+    req.tokenEmail = decoded.email
+    console.log(decoded)
+    next()
+  } catch (err) {
+    console.log(err)
+    return res.status(401).send({ message: 'Unauthorized Access!', err })
+  }
+}
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+async function run() {
+  try {
+
+
+    const db=client.db('productDB')
+    const productCollection=db.collection('products')
+    const ordersCollection = db.collection('orders')
+
+// save a product data in db
+    app.post('/products',async (req,res)=>{
+      const productData=req.body
+      console.log(productData)
+      const result= await productCollection.insertOne(productData)
+      res.send(result)
+    })
+
+    app.get('/products',async (req,res)=>{
+      const result= await productCollection.find().toArray()
+      res.send(result)
+    })
+    // get all products from db
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id
+      const result = await productCollection.findOne({ _id: new ObjectId(id) })
+      res.send(result)
+    })
+    // Payment endpoints
+    app.post('/create-checkout-session', async (req, res) => {
+      const paymentInfo = req.body
+      console.log(paymentInfo)
+       const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currency: 'usd',
+              product_data: {
+                name: paymentInfo?.title,
+                description: paymentInfo?.description,
+                images:paymentInfo.images,
+              },
+              unit_amount: paymentInfo?.price * 100,
+            },
+            quantity: paymentInfo?.quantity,
+          },
+        ],
+        customer_email: paymentInfo?.customer?.email,
+        mode: 'payment',
+        metadata: {
+          productId: paymentInfo?.productId,
+          customer: paymentInfo?.customer.email,
+        },
+        success_url: `${process.env.CLIENT_DOMAIN}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.CLIENT_DOMAIN}/product/${paymentInfo?.productId}`,
+      })
+      res.send({ url: session.url })
+    })
+
+
+    app.post('/payment-success', async (req, res) => {
+      const { sessionId } = req.body
+      const session = await stripe.checkout.sessions.retrieve(sessionId)
+     
+      const product = await productCollection.findOne({
+        _id: new ObjectId(session.metadata.productId),
+      })
+      const order = await ordersCollection.findOne({
+        transactionId: session.payment_intent,
+      })
+
+      if (session.status === 'complete' && product && !order) {
+        // save order data in db
+        const orderInfo = {
+          productId: session.metadata.productId,
+          transactionId: session.payment_intent,
+          customer: session.metadata.customer,
+          status: 'pending',
+          manager: product.manager,
+          title: product.title,
+          category: product.category,
+          quantity: 1,
+          price: session.amount_total / 100,
+          image: product?.images,
+        }
+        const result = await ordersCollection.insertOne(orderInfo)
+        // update plant quantity
+        await productCollection.updateOne(
+          {
+            _id: new ObjectId(session.metadata.productId),
+          },
+          { $inc: { quantity: -1 } }
+        )
+        return res.send({
+          transactionId: session.payment_intent,
+          orderId: result.insertedId,
+        })
+      }
+      res.send(
+        res.send({
+          transactionId: session.payment_intent,
+          orderId: order._id,
+        })
+      )
+      
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 })
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    )
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+  res.send('Hello from Server..')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
